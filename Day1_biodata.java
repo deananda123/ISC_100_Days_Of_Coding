3c11dce859183e0f30224f87a7fb820f92062b10
deananda123 <deanandaa0@gmail.com>
2023-10-09 16:15:35 -1000

    Day1_biodata

---
+++ Day1_biodata.java
@@ -0,0 +1,24 @@
+
+package day1_biodata;
+public class Day1_biodata { 
+    public static void main(String[] args) {
+        String n = "Deananda";
+        String ag = "islam";
+        String j = "perempuan";
+        String f = "teknik";
+        String k = "informatika C";
+        int u = 18;
+        String t = "mamuju/06-07-2005";
+        String  a = "lino maloga";
+        
+        System.out.println("nama: "+n);
+        System.out.println("agama: "+ag);
+        System.out.println("jenis kelamin: "+j);
+        System.out.println("fakultas: "+f);
+        System.out.println("kelas: "+k);
+        System.out.println("umur: "+u + "tahun");
+        System.out.println("tempat/tanggal lahir: "+t);
+        System.out.println("alamat: "+a);
+    }
+    
+}
