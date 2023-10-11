0b8282b95648a85dc32bab486a91802668742eb7
deananda123 <deanandaa0@gmail.com>
2023-10-10 20:22:08 -1000

    aaa

---
+++ Day2_TipedataPrimiti.java
@@ -0,0 +1,31 @@
+
+package day2_tipedataprimiti;
+
+
+public class Day2_TipedataPrimiti {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 10;
+        short b = 18;
+        byte c = 10;
+        long d = 2023;
+        float e = 34.0f;
+        double f= 160.8;
+        char g = 'A';
+        boolean h = true;
+                
+        System.out.println("tanggal: "+ a);
+        System.out.println("umur: "+ b);
+        System.out.println("jam: "+ a);
+        System.out.println("tahun: "+ a);
+        System.out.println("susu dalam fhrenheit: "+ a);
+        System.out.println("berat: "+ a);
+        System.out.println("tinggi: "+ a);
+        System.out.println("semuanya adalah: "+ a);
+        
+
+     }
+    
+}
