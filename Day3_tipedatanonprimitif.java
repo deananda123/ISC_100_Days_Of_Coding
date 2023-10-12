5f1922528e507f0c99b55469daaa26f1216fceb0
deananda123 <deanandaa0@gmail.com>
2023-10-11 19:25:51 -1000

    day3

---
+++ Day3_tipedatanonprimitif.java
@@ -0,0 +1,17 @@
+
+package day3_tipedatanonprimitif;
+
+
+public class Day3_tipedatanonprimitif {
+
+    
+    public static void main(String[] args) {
+        //String
+        String nama = "deananda";
+        //Array
+        String[] namapacar = {"eunhyuk","suho", "dazai", "gojo", "lucas"};
+        
+        
+    }
+    
+}
