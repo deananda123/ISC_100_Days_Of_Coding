a239ba932019ea5548397f68011ef31d626e05b9
deananda123 <deanandaa0@gmail.com>
2023-10-14 19:12:12 -1000

    day6

---
+++ Day6.java
@@ -0,0 +1,29 @@
+
+package day6;
+
+
+public class Day6 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("aritmatika: ");
+        int x =20;
+        int y =5;
+        
+        
+        int penjumlahan = x+y;
+        int pengurangan = x-y;
+        float pembagian=x/y;
+        int perkalian=x*y;
+        int modulus=x%y;
+        
+        System.out.println("hasil penjumlahan: "+penjumlahan);
+        System.out.println("hasil pengurangan: "+pengurangan);
+        System.out.println("hasil pembagian: "+pembagian);
+        System.out.println("hasil perkalian: "+perkalian);
+        System.out.println("hasil sisa bagi: "+modulus);
+        
+        
+    }
+    
+}
