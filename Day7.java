ac098e981e01fc4590cce47f8da25d795f553730
deananda123 <deanandaa0@gmail.com>
2023-10-15 21:19:56 -1000

    day7

---
+++ Day7.java
@@ -0,0 +1,37 @@
+
+package day7;
+
+
+public class Day7 {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("penugasan: ");
+        int x=12;
+        int y=4;
+        int z=3;
+        
+        //penjumlahan
+        x+=y;
+        System.out.println("hasil penjumlahan: "+x);
+        
+        //pengurangan
+        x-=z;
+        System.out.println("hasil pengurangan: "+x);
+        
+        //perkalian
+        x*=y;
+        System.out.println("hasil perkalian: "+x);
+        
+        //pembagian
+        x/=y;
+        System.out.println("hasil penjumlahan: "+x);
+        
+        //modulus
+        x%=z;
+        System.out.println("sisa bagi: "+x);
+               
+    }
+    
+}
