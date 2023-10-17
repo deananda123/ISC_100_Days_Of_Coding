e1a3fc472d8d65135de31c7c18dc88335e9d09c7
deananda123 <deanandaa0@gmail.com>
2023-10-16 22:02:55 -1000

    day8

---
+++ Day8_perbandingan.java
@@ -0,0 +1,32 @@
+
+package day8_perbandingan;
+
+
+public class Day8_perbandingan {
+
+    
+    public static void main(String[] args) {
+        //Operator Pembanding
+        double tinggi1 = 160.8;
+        double tinggi2 = 165.8;
+        
+        System.out.print("a. Tinggi badan 1 dan 2 adalah sama: ");
+        System.out.println(tinggi1 == tinggi2);
+        
+        System.out.print("b. Tinggi badan 1 dan 2 tidak sama: ");
+        System.out.println(tinggi1 != tinggi2);
+        
+        System.out.print("c. Tinggi badan 1 lebih kecil dari tinggi badan 2: ");
+        System.out.println(tinggi1 < tinggi2);
+        
+        System.out.print("d. Tinggi badan 1 lebih kecil sama dengan tinggi badan 2: ");
+        System.out.println(tinggi1 <= tinggi2);
+        
+        System.out.print("e. Tinggi badan 1 lebih besar dari tinggi badan 2: ");
+        System.out.println(tinggi1 > tinggi2);
+        
+        System.out.print("f. Tinggi badan 1 lebih besar sama dengan tinggi badan 2: ");
+        System.out.println(tinggi1 >= tinggi2);
+    }
+    
+}
