5f98f91ca49a6ba92f263ac2cdc03a57cbc136bf
deananda123 <deanandaa0@gmail.com>
2023-10-22 07:42:28 -1000

    aritmatika

---
+++ Day13_.java
@@ -0,0 +1,21 @@
+package Day13;
+
+public class Day13_ {
+    public static void main(String[] args) {
+        //aritmatika
+        int x,y,z,v,a;
+        
+        x=4;
+        y=2;
+        z=8;
+        v=16;
+        
+        a= x*y;
+        System.out.println("hasil  x*y= "+a);
+        a=a+z;
+        System.out.println("x*y=a maka a+z hasilnya: "+a);
+        a=a/v;
+        System.out.println("hasil dari a+z=a maka a/v hasilnya: "+a);
+       
+    }
+}
