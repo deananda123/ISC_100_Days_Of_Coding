e470d76942f0bb3cf7646ebcbce4d168269b5afc
deananda123 <deanandaa0@gmail.com>
2023-10-22 20:33:35 -1000

    day14 percabangan

---
+++ Day14_.java
@@ -0,0 +1,23 @@
+
+package Day14;
+public class Day14_ {
+    public static void main(String[] args) {
+        //pengkondisian
+        int a,b,c,d;
+        a=3;
+        b=7;
+        c=4;
+        d=1;
+        
+        if(a>b){
+            System.out.println("a>d adalah: "+ true);
+        }else if(b==c){
+            System.out.println("b==c adalah: "+true);
+        }else if(c!=d){
+            System.out.println("c!=d adalah: "+true);
+        }
+        
+        
+    }
+    
+}
