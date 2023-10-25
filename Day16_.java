819966743aff5a008b19d3b068513c9a88918bdd
deananda123 <deanandaa0@gmail.com>
2023-10-24 16:28:37 -1000

    day16

---
+++ Day16_.java
@@ -0,0 +1,26 @@
+package Day16;
+public class Day16_ {
+    public static void main(String[] args) {
+        int planet = 1;
+        switch (planet){
+            case 1:
+                System.out.println("Mekurius");
+            case 2:
+                System.out.println("Venus");
+            case 3:
+                System.out.println("Bumi");
+            case 4:
+                System.out.println("Mars");
+            case 5:
+                System.out.println("Jupiter");
+                break;
+            case 6:
+                System.out.println("Saturnus");
+            case 7:
+                System.out.println("Uranus");
+            case 8:
+                System.out.println("Neptunus");
+            
+        }
+    }
+}
