fa1457b666437217c8cb3157952a61e940026f2b
deananda123 <deanandaa0@gmail.com>
2023-10-26 21:47:15 -1000

    day18

---
+++ Day18__.java
@@ -0,0 +1,16 @@
+package Day18_;
+
+public class Day18__ {
+    public static void main(String[] args) {
+    int x=5,y=7;
+ 
+        if(x<y){
+            if(y==7){
+                System.out.println("x<y dan y=7 adalah benar");
+            }else{
+                    System.out.println("kedua pernyataan tidak benar");
+                }
+            }
+        }
+    }
+
