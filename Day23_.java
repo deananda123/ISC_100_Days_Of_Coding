0a17a3852b4e87504e73f17b609a0aa74a444f3b
deananda123 <deanandaa0@gmail.com>
2023-10-31 21:08:50 -1000

    day23

---
+++ Day23_.java
@@ -0,0 +1,19 @@
+package Day23;
+
+import java.util.Scanner;
+
+public class Day23_ {
+    public static void main(String[] args) {
+        Scanner De = new Scanner(System.in);
+        System.out.print("Masukkan batas: ");
+        int x = De.nextInt();
+        int y = 1;
+        
+        while (y<=x){
+            System.out.println("Dea "+y);
+            y++;
+        }
+        
+    }
+    
+}
