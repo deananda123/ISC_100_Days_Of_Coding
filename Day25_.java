0e6595449e12248d991e5762d1e2881c562a684c
deananda123 <deanandaa0@gmail.com>
2023-11-02 20:45:14 -1000

    for

---
+++ Day25_.java
@@ -0,0 +1,10 @@
+package Day25;
+public class Day25_ {
+    public static void main(String[] args) {
+        System.out.println("perulanga bilangan genap");
+        for (int x = 0; x < 10; x+=2) {
+            System.out.println(x);
+        }
+    }
+    
+}
