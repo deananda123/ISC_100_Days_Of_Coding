2bd8d0752aa6bad940f3a7b9a24ad049cc9b5c23
deananda123 <deanandaa0@gmail.com>
2023-11-05 20:25:40 -1000

    persegi

---
+++ Day28_.java
@@ -0,0 +1,13 @@
+package Day28;
+public class Day28_ {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+ 
+        }
+    }
+    
+}
