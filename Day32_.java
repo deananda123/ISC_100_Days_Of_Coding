d073150354f90b42128ec0d09039755a9a9fc9ff
deananda123 <deanandaa0@gmail.com>
2023-11-09 22:29:20 -1000

    array

---
+++ Day32_.java
@@ -0,0 +1,14 @@
+package Day32;
+public class Day32_ {
+    public static void main(String[] args) {
+        int angka[]={1, 10,8,4,9,5,6};
+        System.out.println("index ke 0: "+angka[0]);
+        System.out.println("index ke 1: "+angka[1]);
+        System.out.println("index ke 2: "+angka[2]);
+        System.out.println("index ke 3: "+angka[3]);
+        System.out.println("index ke 4: "+angka[4]);
+        System.out.println("index ke 5: "+angka[5]);
+        System.out.println("index ke 6: "+angka[6]);
+    }
+    
+}
