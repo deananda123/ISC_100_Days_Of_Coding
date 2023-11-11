40d5266d0039acb3b401a9785b9a0d9f7233d532
deananda123 <deanandaa0@gmail.com>
2023-11-10 20:32:43 -1000

    array dua dimensi

---
+++ Day33_.java
@@ -0,0 +1,11 @@
+package Day33;
+
+import java.util.Arrays;
+
+public class Day33_ {
+    public static void main(String[] args) {
+        int [][]array={{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}};
+        System.out.println(Arrays.deepToString(array));
+    }
+    
+}
