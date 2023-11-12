fd033b4b331890fe68d47f8518303b9b0066751f
deananda123 <deanandaa0@gmail.com>
2023-11-11 20:02:47 -1000

    array satu dimensi

---
+++ Day34_.java
@@ -0,0 +1,22 @@
+package Day34;
+
+import java.util.Scanner;
+
+public class Day34_ {
+    public static void main(String[] args) {
+        
+        Scanner De = new Scanner(System.in);
+        System.out.print("size array: ");
+        int a = De.nextInt();
+        String nama[]=new String[a];
+        for (int i = 0; i < a; i++){
+            nama[i]=De.nextLine();
+        }
+        System.out.println("-----outpout-----");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("nama: "+nama[i]);
+            
+        }
+    }
+    
+}
