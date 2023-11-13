3105208039c013585fa0b16cea997632f1e8c37f
deananda123 <deanandaa0@gmail.com>
2023-11-12 20:42:03 -1000

    arrayloop

--- Day34_.java
+++ Day34_.java
@@ -4,16 +4,15 @@ import java.util.Scanner;
 
 public class Day34_ {
     public static void main(String[] args) {
-        
         Scanner De = new Scanner(System.in);
         System.out.print("size array: ");
         int a = De.nextInt();
         String nama[]=new String[a];
-        for (int i = 0; i < a; i++){
+        for (int i = 0; i <a; i++){
             nama[i]=De.nextLine();
         }
         System.out.println("-----outpout-----");
-        for (int i = 0; i < 5; i++) {
+        for (int i = 1; i < a; i++) {
             System.out.println("nama: "+nama[i]);
             
         }
---
+++ Day35_arrayLoop.java
@@ -0,0 +1,13 @@
+package Day35;
+public class Day35_arrayLoop {
+    public static void main(String[] args) {
+        System.out.println("Kru Mugiwara: ");
+        String []onepiece = {"luffy", "zoro","nami","usop","sanji","robin", "chopper","brook","franky","jimbe"};
+        for (String i : onepiece) {
+            
+            System.out.println(i);
+            
+        }
+    }
+    
+}
