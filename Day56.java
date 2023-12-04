2597180aa9063f787c0e5023ac3bd8501f9d24e4
deananda123 <deanandaa0@gmail.com>
2023-12-03 23:37:18 -1000

    method

---
+++ Day56.java
@@ -0,0 +1,13 @@
+package Day56;
+public class Day56 {
+    public static void main(String[] args) {
+        int l = luas(10, 5);
+        System.out.println(l);
+        
+    }
+    private static int luas(int panjang, int lebar){
+        int hasil = panjang*lebar;
+        return hasil;
+    }
+    }
+    
