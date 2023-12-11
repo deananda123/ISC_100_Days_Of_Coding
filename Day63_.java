8552fa28f3713ce025c41344fc2ed6e97f6df0f1
deananda123 <deanandaa0@gmail.com>
2023-12-10 21:02:04 -1000

    Format String

---
+++ Day63_.java
@@ -0,0 +1,18 @@
+package Day63;
+public class Day63_ {
+    public static void main(String[] args) {
+        String nama = "nanda";
+        float bb = 46.55282f;
+        float b = 1.44353f;
+        float b1 = bb - b;
+        System.out.printf("Dia adalah %1$s "
+                + "\n%1s memiliki berat badan sekitar %2$+5.3f"
+                + " \nBerat badan %1$s turun sekitar %3$+5.3f"
+                + " \nSehingga Berat badan %1$s adalah %4$+5.3f",nama,bb,b,b1);
+        
+        
+        
+        
+    }
+    
+}
