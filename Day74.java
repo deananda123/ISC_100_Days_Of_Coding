7dc25b62a6639a87c61feeac9438ad9f4983e6c6
deananda123 <deanandaa0@gmail.com>
2023-12-21 19:37:13 -1000

    ArrayList

---
+++ Day74.java
@@ -0,0 +1,20 @@
+package Day74;
+
+import java.util.ArrayList;
+
+public class Day74 {
+    public static void main(String[] args) {
+        ArrayList<String> nama = new ArrayList<String>();
+        nama.add("Dea");
+        nama.add("Tisa");
+        nama.add("Ayu");
+        nama.add("Serli");
+        nama.add("Wanti");
+        nama.add("Yulia");
+        for (int i = 0; i < nama.size(); i++) {
+            System.out.println(nama.get(i));
+            
+        }
+    }
+    
+}
