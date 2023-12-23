08a0ef415eb413379477e1fe41962bc7b80303c5
deananda123 <deanandaa0@gmail.com>
2023-12-22 20:31:35 -1000

    75

---
+++ Day75.java
@@ -0,0 +1,18 @@
+package Day75;
+
+import java.util.LinkedList;
+
+public class Day75 {
+    public static void main(String[] args) {
+        LinkedList<String> anim = new LinkedList<String>();
+        anim.add("Gojo");
+        anim.add("levi");
+        anim.add("kaneki");
+        anim.add("Kou");
+        anim.add("Uzui");
+        anim.add("Vanitas");
+        System.out.println(anim);
+        
+    }
+    
+}
